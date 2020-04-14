d4a562ada5c8bbd99efd02d76e71539f758196af
Poizd <muhasj@gmail.com>
2020-04-14 21:38:35 +0300

    oh my code

---
+++ webpack.config.prod.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
+  },
+  plugins: [
+    // Generate an external css file with a hash in the fielname
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Hassshhh
+    new WebpackMd5Hash(),
+
+    // Use CommensChunkPlugin to create a separate bundle
+    // of vendor libraies
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
+    // Create HTML file that includes reference to bundle JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyCSS:true,
+        minifyJS: true,
+        minifyURLs:true
+      },
+      inject: true
+    }),
+
+    //Eliminate duplicate packages when generating undle
+    new webpack.optimize.DedupePlugin(),
+
+    //Minify js
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+    ]
+  }
+}
